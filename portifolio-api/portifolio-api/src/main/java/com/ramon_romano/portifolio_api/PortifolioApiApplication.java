94eae4c12312e131bc58aa77d79cb580721c4a26
Ramon Romano Tardetti <Ramon.tardetti@JML.LOCAL>
2026-03-04 16:57:06 -0300

    Commit inicial: Inicio do Portifolio

---
+++ portifolio-api/portifolio-api/src/main/java/com/ramon_romano/portifolio_api/PortifolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramon_romano.portifolio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioApiApplication.class, args);
+	}
+
+}
---
+++ portifolio-api/portifolio-api/src/test/java/com/ramon_romano/portifolio_api/PortifolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon_romano.portifolio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
